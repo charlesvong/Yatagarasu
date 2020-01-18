ec7857dd6fba579a614ab81fb69d1ed7d4ba995a
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-01-18 13:31:39 -0500

    add gate and key script

--- Assets/Scenes/gatePass.cs
+++ Assets/Scenes/gatePass.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class gatePass : MonoBehaviour
 {
+    public playerMovement player;
+    public Transform outPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == player.name) {
+            player.WarpToPosition(outPosition.position);
+        }
     }
 }
