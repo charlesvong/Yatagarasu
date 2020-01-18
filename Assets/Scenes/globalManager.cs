a52555c4ed65eca9c3a2b7f7a2d5507a584ed5c5
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-01-18 14:51:08 -0500

    finish box

--- Assets/Scenes/globalManager.cs
+++ Assets/Scenes/globalManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class globalManager : MonoBehaviour
 {
     private static int key = 0;
+    private static bool passed = false;
+    private static string text;
     public Text logText;
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,30 @@ public class globalManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (key > 0) {
-            logText.text = "key collected";
-        }
+         logText.text = text;
         
+    }
+
+    public static bool haveKey() {
+        return key > 0;
     }
 
     public static void keyCollected() {
         key++;
+        text = "key collected";
+    }
+
+    public static void keyPassed()
+    {
+        passed = true;
+        text = "key passed";
+    }
+
+    public static bool canOpen() {
+        return passed;
+    }
+
+    public static void opened() {
+        text = "box opened";
     }
 }
