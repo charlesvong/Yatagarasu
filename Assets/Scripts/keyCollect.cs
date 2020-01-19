ce0f9e8ba59c104328327208b3ad22a6a08ace1e
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-01-18 22:55:25 -0500

    add missing scripts

---
+++ Assets/Scripts/keyCollect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyCollect : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject player1;
+    public GameObject player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name==player1.name || collision.collider.name == player2.name) {
+            globalManager.keyCollected();
+            Destroy(key);
+        }
+    }
+}
