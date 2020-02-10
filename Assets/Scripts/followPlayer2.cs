8cbed199283a018a22c7f1a0f2cba0e06343101d
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-02-09 23:07:14 -0500

    new brand

---
+++ Assets/Scripts/followPlayer2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer2 : MonoBehaviour
+{
+    public playerMovement player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = player.gameObject.transform.forward;
+        Vector3 factor = new Vector3(-2 * transform.forward.x, 2, -2* transform.forward.z);
+        transform.position = player.gameObject.transform.position + factor;
+
+
+    }
+}
