ffa2ca92dadb3c3636072851de90f428c7236f59
charlesvong <charlesvong2013@gmail.com>
2020-02-14 00:51:23 -0500

    Added character selection to main menu screen

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Rewired;
 
 public class MainMenu : MonoBehaviour
 {
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
