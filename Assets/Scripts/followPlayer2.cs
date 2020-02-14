164262859a3c2647821101c8469a2cc2d3d7e3f6
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-02-13 19:28:02 -0500

    add scrpts

--- Assets/Scripts/followPlayer2.cs
+++ Assets/Scripts/followPlayer2.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class followPlayer2 : MonoBehaviour
 {
-    public playerMovement player;
+    public playerMovement2 player;
     public Vector3 offset;
+    private Player playerCon;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.forward = player.gameObject.transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.forward = player.gameObject.transform.forward;
-        Vector3 factor = new Vector3(-2 * transform.forward.x, 2, -2* transform.forward.z);
+        playerCon = player.getController();
+
+        float viewHorizontal = playerCon.GetAxis("View Horizontal");
+
+        Vector3 factor = new Vector3(offset.x * transform.forward.x, offset.y, offset.z * transform.forward.z);
         transform.position = player.gameObject.transform.position + factor;
+
+        this.transform.RotateAround(player.transform.position, Vector3.up, viewHorizontal* 360 * Time.deltaTime);
 
 
     }
