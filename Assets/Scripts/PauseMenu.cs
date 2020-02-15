aaf098e6bf1f839189324612bbdf0775ea04b561
charlesvong <charlesvong2013@gmail.com>
2020-02-15 00:49:13 -0500

    got pause-screen layout done

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    private playerMovement controller;
+
+    private CharacterController temp;
+    void start()
+    {
+        controller = GetComponent<playerMovement>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(controller);
+        Debug.Log(temp);
+        if(controller.getController().GetButtonDown("Pause"))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+}
