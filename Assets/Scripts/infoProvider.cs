b0ba317d8be7a31dda36780b4a339d36028a1006
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-02-16 01:24:11 -0500

    finish talk restrictions and ai stop during talking

---
+++ Assets/Scripts/infoProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infoProvider : MonoBehaviour
+{
+
+    public int restrict_player_id;
+    private GameObject caller;
+    private bool providing = false;
+    public float talkTime;
+    private float timer;
+
+    // 0 for normal, 1 for attracted, 2 for unconscious
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (providing && timer > 0.0f)
+        {
+            Vector3 targetDirection = caller.transform.position - this.transform.position;
+            float singleStep = 5 * Time.deltaTime;
+            targetDirection.y = 0;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            this.transform.rotation = Quaternion.LookRotation(newDirection);
+            timer -= Time.deltaTime;
+        }
+        else if (providing) {
+            timer = 0;
+            providing = false;
+            endPrividing();
+        }
+        
+    }
+
+    public int getRestrict() {
+        return restrict_player_id;
+    }
+
+    public void provideInfo() {
+        this.GetComponent<AI>().Stand();
+        providing = true;
+        timer = talkTime;
+        Debug.Log("i will give u info");
+    }
+
+    public void endPrividing() {
+        this.GetComponent<AI>().BackToDefault();
+    }
+
+    public void setCaller(GameObject obj) {
+        this.caller = obj;
+    }
+
+}
---
+++ Assets/Scripts/interactableProvider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactableProvider : Interactable
+{
+
+    private int ACTION_PROVIDE = 1;
+    private int ACTION_NOTHING = 0;
+    // Start is called before the first frame update
+
+    private infoProvider provider;
+
+    void Start()
+    {
+        provider = this.GetComponent<infoProvider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public string getInstructions(int player_id)
+    {
+        if (player_id == provider.getRestrict())
+        {
+            return "Press B to get information";
+        }
+        else {
+            return "";
+        }
+    }
+
+    override public int getActionCode(int player_id, GameObject player_obj)
+    {
+        if (player_id == provider.getRestrict())
+        {
+            provider.setCaller(player_obj);
+            return ACTION_PROVIDE;
+        }
+        else
+        {
+            return ACTION_NOTHING;
+        }
+    }
+
+    override public void interact(int actionCode)
+    {
+        if (actionCode == ACTION_PROVIDE)
+        {
+            provider.provideInfo();
+        }
+    }
+}
