9200ed4bca614fb4abfd6d44a62fb37d5e41cc51
Zhifan Wang <zhifan.wang@mail.utoronto.ca>
2020-02-15 22:28:28 -0500

    finish split screen and map indicators

---
+++ Assets/Scripts/createIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createIndicator : MonoBehaviour
+{
+
+    public mapIndicator myPrefab;
+    public Transform map;
+    public Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myPrefab = Instantiate(myPrefab, this.transform.position, Quaternion.identity);
+        myPrefab.target = this.transform;
+        myPrefab.map = map;
+        myPrefab.GetComponent<Renderer>().material = mat;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/screenSizeAdj.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class screenSizeAdj : MonoBehaviour
+{
+
+    public float factor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Screen.width / 4 > Screen.height / 3)
+        {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.height * factor / 3 * 4, Screen.height * factor);
+        }
+        else {
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width * factor , Screen.width * factor / 4 * 3);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
