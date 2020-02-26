4044242175f80bb8ed684461af5eda41048e775d
charlesvong <charlesvong2013@gmail.com>
2020-02-25 19:28:53 -0500

    Integrated banter within the game

---
+++ Assets/Scripts/FungusDialogue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusDialogue : MonoBehaviour
+{
+    public Flowchart flowchart;
+    private List<int> Banterlist = new List<int>{1,2,3,4,5};
+    // Start is called before the first frame update
+    public void banter(){
+        int randomNum = Random.Range(Banterlist[0], Banterlist.Count + 1);
+        string blockName = "Banter" + randomNum.ToString();
+        Debug.Log(blockName);
+        Banterlist.Remove(randomNum);
+        flowchart.ExecuteBlock(blockName);
+    }
+
+    public void TimeAlmostUp(){
+        flowchart.ExecuteBlock("Hurry");
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Rewired;
+using Fungus;
 
 public class Timer : MonoBehaviour
 {
@@ -14,11 +15,16 @@ public class Timer : MonoBehaviour
     private bool selected = false;
 
     private Button firstSelected;
+    private bool firstBanter = false;
+    private bool secondBanter = false;
+    private bool HurryUpReminder = false;
+    private FungusDialogue dialogue;
 
     // Start is called before the first frame update
     void Start()
     {
         timer = maxTimer;
+        dialogue = GetComponent<FungusDialogue>();
     }
 
     // Update is called once per frame
@@ -32,7 +38,18 @@ public class Timer : MonoBehaviour
             string readableTime = string.Format("{0:0}:{1:00}", minutes, seconds);
             uiText.text = readableTime;
         }
-
+        if(timer <= 180f && !firstBanter){
+            dialogue.banter();
+            firstBanter = true;
+        }
+        if(timer <= 120f && !secondBanter){
+            dialogue.banter();
+            secondBanter = true;
+        }
+        if(timer <= 60f && !HurryUpReminder){
+            dialogue.TimeAlmostUp();
+            HurryUpReminder = true;
+        }
         // Displays Game Over screen when timer goes to 0
         else if(timer <= 0.5f)
         {
