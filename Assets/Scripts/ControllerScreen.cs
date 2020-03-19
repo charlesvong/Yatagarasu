dc1f0d45613c56a64b91cb62c96f7677ac39d1be
charlesvong <charlesvong2013@gmail.com>
2020-03-18 20:44:58 -0400

    added in between scene explaining controls

---
+++ Assets/Scripts/ControllerScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Rewired;
+
+public class ControllerScreen : MonoBehaviour
+{
+    private MainMenu MM;
+    void Start(){
+        MM = GetComponent<MainMenu>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < ReInput.players.playerCount; i++)
+        {
+            Player player = ReInput.players.Players[i];
+            if(player.GetButtonDown("Start")){
+                MM.PlayGame();
+            }
+        }
+    }
+}
